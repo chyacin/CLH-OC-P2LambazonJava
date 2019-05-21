2934de693d2d310b34b3413cd079458934135060
calvinhyacinth <calvinhyacinth@192.168.0.20>
2019-05-21 10:42:49 +0200

    The return type was changed from to Array to list to facilitate the changes which was made before.

--- src/main/java/com/openclassrooms/shop/repository/ProductRepository.java
+++ src/main/java/com/openclassrooms/shop/repository/ProductRepository.java
@@ -40,9 +40,10 @@ public class ProductRepository {
     /**
      * @return All products from the inventory
      */
-    public Product[] findAll()
+    public List<Product> findAll()
     {
-        return products.stream().filter(p -> p.getStock() > 0).sorted(Comparator.comparing(Product::getName)).toArray(Product[]::new);
+        //CH :This was changed from array to List
+    	return products.stream().filter(p -> p.getStock() > 0).sorted(Comparator.comparing(Product::getName)).collect(Collectors.toList());
     }
 
     /**
